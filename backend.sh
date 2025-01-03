1bd0ac4daf8c54a342a491a9a4adeaeed6f81ee4
Avinash Kumar Singh <acavinashchauhan5@gmail.com>
2025-01-03 22:43:57 +0530

    location changed in backend service

--- backend.sh
+++ backend.sh
@@ -79,7 +79,7 @@ npm install &>>$LOGFILE
 
 VALIDATE $? "Installing dependencies..."
 
-cp /home/ec2-user/expense-shell/backend.service /etc/systemd/system/backend.service &>>$LOGFILE
+cp /home/ec2-user/expense-app/backend.service /etc/systemd/system/backend.service &>>$LOGFILE
 
 VALIDATE $? "Copying backend.service..."
 
